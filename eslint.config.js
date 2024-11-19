2a7d0f057c1f90059e0429e5b6eea3141bcf9d8d
Kenji Saito <ken-yo@mbr.nifty.com>
2024-11-19 16:27:58 +0900

    Bumps node modules

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,6 @@
 import eslint from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 import stylisticTs from '@stylistic/eslint-plugin-ts';
-import stylisticJsx from '@stylistic/eslint-plugin-jsx';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
@@ -28,12 +27,10 @@ export default tseslint.config(
     plugins: {
       '@stylistic': stylistic,
       '@stylistic/ts': stylisticTs,
-      '@stylistic/jsx': stylisticJsx,
     },
     rules: {
       '@stylistic/semi': 'error',
       '@stylistic/ts/indent': ['error', 2],
-      '@stylistic/jsx/jsx-indent': ['error', 2],
     },
   },
 );
